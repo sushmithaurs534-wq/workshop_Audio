7102cc63e313880699152f72df5a5f86eaa69965
sushmithaurs534-wq <sushmithaurs534@gmail.com>
2026-05-08 15:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
 
 def main():
     st.image("wallpaper.webp")
-    st.title(" PragyanAI - VVIET Workshop: Audio Hub")
+    st.title(" Nature - Beauty")
     
     audio_bytes = audio_recorder(text="Click to record", neutral_color="#6aa36f")
     
